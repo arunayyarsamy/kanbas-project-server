1cb018fef71456a0a17c4102f9fb19e85cc28deb
Ashwin Sathiyanarayanan <66624673+ash-02@users.noreply.github.com>
2024-04-24 01:19:48 -0400

    Backend changes

--- Kanbas/Quizzes/QuizPreview/routes.js
+++ Kanbas/Quizzes/QuizPreview/routes.js
@@ -3,7 +3,9 @@ import * as dao from "./dao.js";
 function QuizPreviewRoutes(app) {
 
     const previewQuiz = async (req, res) => {
-        const quizPreview = await dao.findAttemptWithUsernameAndQuiz(req.session["currentUser"].username, req.params.qid);
+        const username = req.session["currentUser"] !== undefined ? req.session["currentUser"].username : "anonymous";
+        // const quizPreview = await dao.findAttemptWithUsernameAndQuiz(req.session["currentUser"].username, req.params.qid);
+        const quizPreview = await dao.findAttemptWithUsernameAndQuiz(username, req.params.qid);
         if (quizPreview === null) {
             res.status(206).send("No preview found");
             return;
@@ -11,7 +13,7 @@ function QuizPreviewRoutes(app) {
         res.json(quizPreview);
     }
 
-    app.get("/quizzes/:qid/preview", previewQuiz);
+    app.get("/api/quizzes/:qid/preview", previewQuiz);
 };
 
 export default QuizPreviewRoutes;
